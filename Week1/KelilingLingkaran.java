293b3c2e573d31e7e85b6fd8bd6ee9405a5411aa
AlfebrianIvo <126554498+AlfebrianIvo@users.noreply.github.com>
2023-03-10 16:03:09 +0700

    Create KelilingLingkaran.java

---
+++ Week1/KelilingLingkaran.java
@@ -0,0 +1,9 @@
+public class KelilingLingkaran {
+    public static void main(String[] args) {
+        double diameter = 10;
+        double jariJari = 10 / 2;
+        double keliling = 2 * 3.14 * 5;
+        
+        System.out.println("Keliling lingkaran dengan diameter 10 adalah " + keliling);
+    }
+}

67d66d94c5d7d1eb8416e63543658292468559a4
AlfebrianIvo <126554498+AlfebrianIvo@users.noreply.github.com>
2023-03-10 16:08:34 +0700

    Create HitungVolumeTabung.java

---
+++ Week1/HitungVolumeTabung.java
@@ -0,0 +1,10 @@
+public class HitungVolumeTabung {
+    public static void main(String[] args) {
+        double diameter = 5;
+        double jariJari = 5 / 2;
+        double tinggi = 10;
+        double volume = 3.14 * 2.5 * 2.5 * 10;
+
+        System.out.println("Volume tabung dengan diameter " + diameter + " dan tinggi " + tinggi + " adalah " + volume);
+    }
+}
